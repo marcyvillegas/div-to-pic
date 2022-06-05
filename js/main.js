7f5eacf77618af2c2b76fb076b8ef3932e420fb5
Marcy Villegas <58241136+marcyvillegas@users.noreply.github.com>
2022-06-05 20:24:17 +0800

    added logic content

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ const wholeContainer = document.querySelector(".whole");
 
 const displayCanvas = async () => {
 
+    console.log("wait");
     const canvas = await html2canvas(document.querySelector(".capture"))
+    console.log("alright");
     wholeContainer.appendChild(canvas)
 }
 
