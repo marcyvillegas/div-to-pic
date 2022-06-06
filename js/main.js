2c5407a039c7c514ade0e288203a558361881fa6
Marcy Villegas <58241136+marcyvillegas@users.noreply.github.com>
2022-06-06 18:52:37 +0800

    added remove button for canvas

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
 
-/**
- * 
- */
+// Declare variable for the exstingn html elements
 const wholeContainer = document.querySelector(".whole");
+const divToCapture = document.querySelector(".capture");
 
+// Declare variables for the newly created elements
+let addedCanvas;
+let removeButton;
 
+/**
+ * Async function that displays the captured canvas
+ */
 const displayCanvas = async () => {
 
+    // returns canvas
     console.log("wait");
-    const canvas = await html2canvas(document.querySelector(".capture"))
+    const canvas = await html2canvas(divToCapture);
     console.log("alright");
-    wholeContainer.appendChild(canvas)
+
+    // add remove button
+    removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+
+    // append canvas to div container
+    addedCanvas = wholeContainer.appendChild(canvas);
+    wholeContainer.appendChild(removeButton);
 }
 
+/**
+ * Removes the displayed canvas
+ */
+const removeCanvas = () => {
+
+    
+}
